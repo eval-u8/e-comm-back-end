477f428c967494f7314fa4d2eae33279f9e1b189
Ernesto Valecillos <evalecillos@gmail.com>
2021-11-06 11:01:17 -0500

    fixed another missing include and typo on product route

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -34,7 +34,18 @@ router.get('/:id', (req, res) => {
   Product.findOne({
     where: {
       id: req.params.id
-    }
+    },
+    attributes:['id','product_name','price','stock'],
+    include: [
+      {
+        model: Category,
+        attributes: ['category_name']
+      },
+      {
+        model: Tag,
+        attributes: ['tag_name']
+      }
+    ]
   })
       .then((dbProductData) => {
         if(!dbProductData){
@@ -131,7 +142,7 @@ router.delete('/:id', (req, res) => {
   })
   .then(dbProductData => {
     if (!dbProductData) {
-      res.status(404).json({message: "No post found with this id"});
+      res.status(404).json({message: "No product found with this id"});
       return
     }
     res.json(dbProductData);
