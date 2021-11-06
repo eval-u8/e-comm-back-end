91b0e81d58f203ebb4c25bb3c4450ea178b3a815
Ernesto Valecillos <evalecillos@gmail.com>
2021-11-06 11:37:21 -0500

    typos

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -37,9 +37,7 @@ router.get('/:id', (req, res) => {
 
 router.post('/', (req, res) => {
   // create a new tag
-  Tag.create({
-    tag_name: req.body.tag_name
-  })
+  Tag.create(req.body)
     .then(dbTagData => res.json(dbTagData))
     .catch(err => {
       console.log(err);
