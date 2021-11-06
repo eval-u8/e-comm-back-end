73049e59021094605385d8332f8034182622a9ec
Ernesto Valecillos <evalecillos@gmail.com>
2021-11-06 10:49:02 -0500

    added index js

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -1,10 +1,15 @@
 const router = require('express').Router();
 const { Category, Product } = require('../../models');
+const { restore } = require('../../models/Category');
 
 // The `/api/categories` endpoint
 
 router.get('/', (req, res) => {
   // find all categories
+  Category.findAll().then(dbCatergoryData => res.json(dbCatergoryData)).catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  })
   // be sure to include its associated Products
 });
 
