0abe8c483f48f2355e858c25b68fdb161b4f95d5
Ernesto Valecillos <evalecillos@gmail.com>
2021-11-06 11:30:29 -0500

    forgot to add through on index models, and did tag routes as well

--- models/index.js
+++ models/index.js
@@ -16,11 +16,13 @@ const ProductTag = require('./ProductTag');
 
 // Products belongToMany Tags (through ProductTag)
   Product.belongsToMany(Tag, {
+    through: ProductTag,
     foreignKey: "product_id",
   })
 
 // Tags belongToMany Products (through ProductTag)
 Tag.belongsToMany(Product, {
+  through: ProductTag,
   foreignKey: "tag_id",
 })
 
